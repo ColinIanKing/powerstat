e2c25c72ddbef6c91c90ec4d91cf030359efc0ea
Colin Ian King <colin.king@canonical.com>
2012-06-14 10:02:57 +0100

    Update copyright year in source
    Signed-off-by: Colin Ian King <colin.king@canonical.com>

--- powerstat.c
+++ powerstat.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Canonical
+ * Copyright (C) 2011-2012 Canonical
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
