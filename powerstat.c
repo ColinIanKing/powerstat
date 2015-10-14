ColinIanKing/powerstat

commit 52f94f4a243a18491486460ef244a331f2e4ec4c

Fix allocation of log_item, as allocating with incorrect struct
Fortunately the allocation size was the same as the intended struct
allocation size so this error was not causing any faults, however
we should use the correct structure when specifying the size to
calloc.

Signed-off-by: Colin Ian King <colin.king@canonical.com>

powerstat.c
@@ -569,7 +569,7 @@ static int log_printf(const char *const fmt, ...)
 	(void)vsnprintf(buffer, sizeof(buffer), fmt, ap);
 	va_end(ap);
 
-	if ((log_item = calloc(1, sizeof(log_t))) == NULL) {
+	if ((log_item = calloc(1, sizeof(log_item))) == NULL) {
 		fprintf(stderr, "Out of memory allocating log item.\n");
 		return -1;
 	}

commit 56ed76cc65709a1db49bb345e92d070d67b3a616

allocate log item with correct size (LP: #1506168)
2nd time around, lets allocate this with the correct size

Fixes: 52f94f4a243a1849148 ("Fix allocation of log_item, as allocating with incorrect struct")
Signed-off-by: Colin Ian King <colin.king@canonical.com>

powerstat.c
@@ -569,7 +569,7 @@ static int log_printf(const char *const fmt, ...)
 	(void)vsnprintf(buffer, sizeof(buffer), fmt, ap);
 	va_end(ap);
 
-	if ((log_item = calloc(1, sizeof(log_item))) == NULL) {
+	if ((log_item = calloc(1, sizeof(log_item_t))) == NULL) {
 		fprintf(stderr, "Out of memory allocating log item.\n");
 		return -1;
 	}